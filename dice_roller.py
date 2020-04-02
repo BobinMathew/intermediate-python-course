e85350f13576c335f6c783fcaad83c7be607b81b
Bobin Mathew <bobinm3@gmail.com>
2020-04-02 14:21:30 -0400

    ramdom number popups and totals values of 2 rolls are added

--- dice_roller.py
+++ dice_roller.py
@@ -1,6 +1,14 @@
+import random
+
 def main():
-    die = 5;
-    print(f'You rolled a {die}')
+
+    dice_roll = 2;
+    dice_sum = 0;
+    for i in range(0,dice_roll):
+        roll = random.randint(1,6);
+        dice_sum += roll;
+        print(f'You rolled a {roll}');
+    print(f'Yo have rolled a total of {dice_sum}');
 
 if __name__== "__main__":
   main()
